4d27fc9dc4f8407cec774794ffc7ccf9d99fab0f
kalakhhata <ompatel2501@gmail.com>
2024-09-06 11:06:15 -0400

    Here I just learned how we can send props as a array or imteger value and present it

--- props/src/App.js
+++ props/src/App.js
@@ -5,7 +5,7 @@ export default function App()
 {
     return(
         <section>
-            <Joke setup="Why don’t skeletons fight each other?" punchline="They don’t have the guts!" />
+            <Joke setup="Why don’t skeletons fight each other?" punchline="They don’t have the guts!" upvotes={2} downvotes={0}  />
             <Joke setup="Why was the math book sad?" punchline="It had too many problems." />
             <Joke setup="Why don’t eggs tell jokes?" punchline="They’d crack each other up!" />
             <Joke setup="Why did the scarecrow win an award?" punchline="Because he was outstanding in his field!" />
--- props/src/components/Joke.js
+++ props/src/components/Joke.js
@@ -5,6 +5,8 @@ export default function Joke(props){
         <section>
             <h1>Setup : {props.setup}</h1>
             <h4>Punchline : {props.punchline} </h4>
+            {props.upvote && <p>Upvotes : {props.upvote}</p>}
+            {props.downvotes && <p>Downvotes : {props.downvote}</p>}
         </section>
     )
 }
